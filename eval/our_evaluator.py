f89827a85aa1b83a874dc13cd8d4050ddc340cd0
yumuzhihan <1573252900@qq.com>
2024-06-05 16:38:28 +0800

    增加部分评价指标

--- eval/our_evaluator.py
+++ eval/our_evaluator.py
@@ -16,7 +16,8 @@ class OurEvaluator(Evaluator):
         super().__init__(config_path)
         self.config = OurEvaluateConfig(**self.config)
         self.recall_ans = self.load_recall_ans()
-        return
+        self.generate_res = []
+        self.zipped_generate_res = []
 
     def load_recall_ans(self) -> List[Dict[str, List[str] | str]]:
         with open(self.config.recall_json_path, "r") as f:
@@ -43,16 +44,15 @@ class OurEvaluator(Evaluator):
         )
 
         eval_res = {}
-        eval_res["hitrate"] = (
-            self.hitrate(recall_res) * 100
-        )  # 召回结果在所有相关文档中的命中率，只要其中包含一个相关文档，或者是相关文档的一部分，就算命中
+        eval_res["retrieve_recall"] = self.retrieve_recall(recall_res) * 100
+        eval_res["recall_precision"] = self.recall_precision(recall_res) * 100
+        eval_res["content_precision"] = self.content_precision(recall_res) * 100
+        eval_res["answer_sim"] = self.answer_sim(app, recall_res) * 100
+        eval_res["zipped_answer_sim"] = self.zipped_answer_sim(app, recall_res) * 100
         eval_res["answer_accuracy"] = self.answer_accuracy(app, recall_res) * 100
-        eval_res["content_accuracy"] = self.content_accuracy(recall_res) * 100
-        eval_res["zipped_answer_accuracy"] = (
-            self.zipped_answer_accuracy(app, recall_res) * 100
-        )
+        eval_res["qa_revelance"] = self.qa_relevance(app, recall_res) * 100
 
-    def hitrate(self, recall_res: List[List[Document]]) -> float:
+    def retrieve_recall(self, recall_res: List[List[Document]]) -> float:
         assert len(recall_res) == len(self.recall_ans)
         total_len = len(recall_res)
         hit = 0
@@ -70,9 +70,30 @@ class OurEvaluator(Evaluator):
             hit += current_hit
         return hit / total_len
 
-    def answer_accuracy(
-        self, app: AppRegister, recall_res: List[List[Document]]
-    ) -> float:
+    def recall_precision(self, recall_res: List[List[Document]]) -> float:
+        assert len(recall_res) == len(self.recall_ans)
+        total_len = len(recall_res)
+        acc = 0.0
+
+        for i, each_query_recall in enumerate(recall_res):
+            if i % 500 == 0:
+                RagLogger().get_logger().info(
+                    f"Calculating recall accuracy: {i}/{total_len}..."
+                )
+
+            current_acc = 0
+            for i, doc in enumerate(each_query_recall):
+                is_hit = False
+                for ans in self.recall_ans[i]["recall_ans"]:
+                    if doc.page_content in ans or ans in doc.page_content:
+                        is_hit = True
+                        break
+                if is_hit:
+                    current_acc += 1 / len(self.recall_ans[i]["recall_ans"])
+            acc += current_acc
+        return acc / total_len
+
+    def answer_sim(self, app: AppRegister, recall_res: List[List[Document]]) -> float:
         assert len(recall_res) == len(self.recall_ans)
         total_len = len(recall_res)
         acc = 0.0
@@ -83,12 +104,13 @@ class OurEvaluator(Evaluator):
                 )
             zipped_prompt = app.get_prompt(self.query[i], each_query_recall)
             response = app.get_response(zipped_prompt)
+            self.generate_res.append(response)
             response_embedding = app.get_embedding(response)
             ans_embedding = app.get_embedding(self.response[i])
             acc += 1 - cosine(response_embedding, ans_embedding)
         return acc / total_len
 
-    def content_accuracy(self, recall_res: List[List[Document]]) -> float:
+    def content_precision(self, recall_res: List[List[Document]]) -> float:
         total_len = len(recall_res)
         acc = 0.0
 
@@ -111,9 +133,8 @@ class OurEvaluator(Evaluator):
             acc += current_acc
         return acc / total_len
 
-    def zipped_answer_accuracy(
-        self, app: AppRegister, recall_res: List[List[Document]]
-    ):
+    def zipped_answer_sim(self, app: AppRegister, recall_res: List[List[Document]]):
+        assert len(recall_res) == len(self.recall_ans)
         total_len = len(recall_res)
         acc = 0.0
 
@@ -124,8 +145,41 @@ class OurEvaluator(Evaluator):
                 )
             zipped_prompt = f"请你根据下面的信息:{app.zip_prompt(self.query[i], each_query_recall)}\n回答问题:{self.query[i]}"
             response = app.get_response(zipped_prompt)
+            self.zipped_generate_res.append(response)
             response_embedding = app.get_embedding(response)
             ans_embedding = app.get_embedding(self.response[i])
             acc += 1 - cosine(response_embedding, ans_embedding)
 
         return acc / total_len
+
+    def answer_accuracy(self, app: AppRegister, recall_res: List[List[Document]]):
+        assert len(recall_res) == len(self.recall_ans)
+        total_len = len(recall_res)
+        right_num = 0
+
+        for i in range(total_len):
+            ans = self.response[i]
+            generate_res = self.generate_res[i]
+            ans_embedding = app.get_embedding(ans)
+            generate_res_embedding = app.get_embedding(generate_res)
+            dis = cosine(ans_embedding, generate_res_embedding)
+            if dis < 0.3:
+                right_num += 1
+
+        return right_num / total_len
+
+    def qa_relevance(self, app: AppRegister, recall_res: List[List[Document]]):
+        assert len(recall_res) == len(self.recall_ans)
+        total_len = len(recall_res)
+
+        revelance = 0.0
+
+        for i in range(total_len):
+            ans = self.response[i]
+            generate_res = self.generate_res[i]
+            ans_embedding = app.get_embedding(ans)
+            generate_res_embedding = app.get_embedding(generate_res)
+            dis = cosine(ans_embedding, generate_res_embedding)
+            reversed += 1 - dis
+
+        return revelance / total_len
